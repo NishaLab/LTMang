82913178d59f6dc950953f7e27d4705bc6c55c8f
namnd0210 <57388883+namnd0210@users.noreply.github.com>
2020-11-01 06:21:11 -0800

    export file text (#8)
    Co-authored-by: ducnam <namnd0210>

--- src/Server/Test.java
+++ src/Server/Test.java
@@ -19,16 +19,35 @@ public class Test {
         GameDAO gd = new GameDAO();
         Game game = new Game();
         Player player = new Player(1, "123456");
-        Question question = new Question(1, "title", "123", "!23", "!23", "!23", "!23", 1);
-        PlayedQuestion pq = new PlayedQuestion();
-        pq.setChosenAnswer(1);
-        pq.setIsCorrect(true);
-        pq.setQuestion(question);
-        pq.setTime(10);
+        //add question 
+        Question question1 = new Question(1, "title1", "123", "!23", "!23", "!23", "!23", 1);
+        PlayedQuestion pq1 = new PlayedQuestion();
+        pq1.setChosenAnswer(1);
+        pq1.setIsCorrect(true);
+        pq1.setQuestion(question1);
+        pq1.setTime(10);
+
+        Question question2 = new Question(1, "title2", "asdas", "!asdasd", "!asdads", "!asd", "!23", 3);
+        PlayedQuestion pq2 = new PlayedQuestion();
+        pq2.setChosenAnswer(1);
+        pq2.setIsCorrect(true);
+        pq2.setQuestion(question2);
+        pq2.setTime(10);
+
+        Question question3 = new Question(1, "title3", "asd", "!assd", "!asd", "!asdasd", "!assd", 2);
+        PlayedQuestion pq3 = new PlayedQuestion();
+        pq3.setChosenAnswer(1);
+        pq3.setIsCorrect(true);
+        pq3.setQuestion(question3);
+        pq3.setTime(10);
+
         Session session = new Session();
         session.setPlayer(player);
         ArrayList<PlayedQuestion> q = new ArrayList<>();
-        q.add(pq);
+        q.add(pq1);
+        q.add(pq2);
+        q.add(pq3);
+
         session.setQuestion(q);
         game.setPlayDate(new Date());
         ArrayList<Session> ss = new ArrayList<>();
