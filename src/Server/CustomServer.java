daa4d6a3270c47e12176d4596392e51a27e2cb7c
NSQA <nguyensyquanganh@gmail.com>
2020-11-02 10:00:41 +0700

    Add SwingWorker to execute countdown time and gui update, but it still blocks EDT :)

--- src/Server/CustomServer.java
+++ src/Server/CustomServer.java
@@ -8,12 +8,16 @@ package Server;
 import DAO.QuestionDAO;
 import Helper.CountdownHelper;
 import Model.Question;
+import com.mysql.fabric.Server;
 
+import javax.swing.*;
+import javax.swing.Timer;
+import java.awt.event.ActionEvent;
+import java.awt.event.ActionListener;
 import java.io.*;
-import java.lang.reflect.Array;
-import java.text.*;
 import java.util.*;
 import java.net.*;
+import java.util.concurrent.ExecutionException;
 
 /**
  * @author LEGION
@@ -32,37 +36,214 @@ public class CustomServer {
     private static HashMap<Socket, ObjectInputStream> inStreamMap;
 
     //check if time to connect to server is over
-    static boolean isOver;
+    private boolean isOver;
     final static int port = 5056;
-    public final static int clientEndPort = 2112;
+    public final static int clientEndPort = 2113;
+    private final ServerFrame frame;
+    private ActionListener countdown;
 
     final static int timeAnswerQuest = 15; //sec
     public final static int timeConnectToServer = 10; //sec
 
-    private static void questionCountdown(int time) throws InterruptedException, IOException {
+    public CustomServer(ServerFrame frame) {
+        this.frame = frame;
+    }
+
+    private void questionCountdown(int time) throws InterruptedException, IOException {
         System.out.println("You have 15 seconds to send your answer to server...");
+
         while (time > 0) {
             System.out.println(time);
+            frame.getCounter().setText(Integer.toString(time));
+
             Thread.sleep(1000);
             time--;
         }
 
         System.out.println("Time up!");
+        frame.getQuestion().setText("Time up");
     }
 
-    public static void startTimeCountdown(int time) throws InterruptedException, IOException {
-        System.out.println("You have 10 seconds to connect to server...");
-        while (time > 0) {
-            System.out.println(time);
-            Thread.sleep(1000);
-            time--;
+    public void setOver(boolean over) {
+        isOver = over;
+    }
+
+    private int timeLeft;
+
+    public void startTimeCountdown(int time) throws InterruptedException, IOException {
+        timeLeft = time;
+        SwingWorker worker = new SwingWorker() {
+            @Override
+            protected String doInBackground() throws Exception {
+                for (int i = timeLeft; i >= 0; i--) {
+                    publish(i);
+                    Thread.sleep(1000);
+                    System.out.println(i);
+                }
+
+                isOver = true;
+                new Socket(InetAddress.getLocalHost(), port);
+                return "Connection is closed.";
+            }
+
+            @Override
+            protected void process(List chunks) {
+                int val = (int) chunks.get(chunks.size() - 1);
+
+                frame.getCounter().setText(String.valueOf(val));
+            }
+
+            @Override
+            protected void done() {
+                try {
+                    String statusMsg = (String) get();
+                    System.out.println(statusMsg);
+                    frame.getQuestion().setText(statusMsg);
+                } catch (InterruptedException e) {
+                    e.printStackTrace();
+                } catch (ExecutionException e) {
+                    e.printStackTrace();
+                }
+            }
+        };
+        worker.execute();
+
+    }
+
+    public void getConnections(ServerSocket ss) throws IOException {
+
+
+        frame.getQuestion().setText("Game start on port " + port);
+
+        Socket client = null;
+        client = ss.accept();
+        clients.add(client);
+        System.out.println("Client " + client.getPort() + " connected to server.");
+
+    }
+
+    public void start() throws IOException, InterruptedException {
+        //Init
+        ObjectInputStream dis;
+        ObjectOutputStream dos;
+        clients = new ArrayList<>();
+        clientHandlers = new ArrayList<>();
+        clientAnswers = new ArrayList<>();
+        inStreamMap = new HashMap<>();
+        outStreamMap = new HashMap<>();
+//        ServerSocket ss = null;
+//
+//        ss = new ServerSocket(port);
+//        frame.getQuestion().setText("Game start on port " + port);
+
+        QuestionDAO questionDAO = new QuestionDAO();
+        ArrayList<Question> question = questionDAO.getAllQuestion();
+
+        ServerSocket ss = null;
+
+        ss = new ServerSocket(port);
+
+
+        startTimeCountdown(timeConnectToServer);
+
+        while (!isOver) {
+            System.out.println(isOver);
+            getConnections(ss);
         }
-        isOver = true;
+
+        System.out.println("hello");
+        //remove the last element
+        if (clients.size()>0) clients.remove(clients.size()-1);
+
+        frame.getQuestion().setText("Let's start!");
         System.out.println("Server connect closed.");
+
+//        //Just receive client in 10s
+//        while (true) {
+//            if (isOver) break;
+//            Socket client = null;
+//            client = ss.accept();
+//            if (client.getPort() != clientEndPort) {
+//                clients.add(client);
+//                System.out.println("Client " + client.getPort() + " connected to server.");
+//            }
+//        }
+
+        //Inputstreams, outputstreams' initialization
+        if (clients.size() == 0) {
+            System.out.println("No player.");
+            return;
+        }
+        for (Socket client : clients) {
+            dis = new ObjectInputStream(client.getInputStream());
+            dos = new ObjectOutputStream(client.getOutputStream());
+            inStreamMap.put(client, dis);
+            outStreamMap.put(client, dos);
+        }
+
+        //Loop through questions list
+        for (Question q : question) {
+
+            System.out.println(q);
+            //System.out.println("Size before clear: " + clientHandlers.size());
+            clientHandlers.clear();
+            //System.out.println("Size after clear: " + clientHandlers.size());
+
+            for (Socket client : clients) {
+                System.out.println(client);
+                dis = inStreamMap.get(client);
+                dos = outStreamMap.get(client);
+                ClientHandler thread = new ClientHandler(dos, dis, client, q);
+                clientHandlers.add(thread);
+            }
+
+
+            for (ClientHandler ch : clientHandlers) {
+                ch.start();
+            }
+            System.out.println("Get ready to answer " + q.getTitle() + "...");
+
+            try {
+                questionCountdown(timeAnswerQuest);
+                for (ClientHandler ch : clientHandlers) {
+                    dos = ch.getDos();
+//                    System.out.println(ch.getClient().getPort() + ": " + ch.getResponse());
+//                    System.out.println(ch.getClient().getPort() + ": " + ch.getState());
+                    if (ch.getResponse() == null && ch.getState() == Thread.State.TERMINATED) {
+                        Socket toBeRemoved = ch.getClient();
+                        clients.remove(toBeRemoved);
+                        inStreamMap.remove(toBeRemoved);
+                        continue;
+                    }
+                    dos.writeUTF("Time out");
+                    //System.out.println(ch.getDis());
+                    dos.flush();
+
+
+                }
+                //System.out.println("hi");
+                clientAnswerHandler(q);
+                System.out.println("Waiting for 3 seconds to continue...");
+                System.out.println("Size: " + clientAnswers.size());
+
+
+            } catch (InterruptedException e) {
+                e.printStackTrace();
+            } catch (SocketException e) {
+                System.out.println("No more client");
+            }
+        }
+
+        System.out.println("Answers: ");
+        for (String ans : clientAnswers) {
+            System.out.println(ans);
+        }
+
+
+        System.out.println("Game over.");
     }
 
-
-    public static void main(String[] args) throws IOException {
+    /*public static void main(String[] args) throws IOException {
         //Init
         ObjectInputStream dis;
         ObjectOutputStream dos;
@@ -93,6 +274,10 @@ public class CustomServer {
         }
 
         //Inputstreams, outputstreams' initialization
+        if (clients.size() == 0) {
+            System.out.println("No player.");
+            return;
+        }
         for (Socket client : clients) {
             dis = new ObjectInputStream(client.getInputStream());
             dos = new ObjectOutputStream(client.getOutputStream());
@@ -104,9 +289,9 @@ public class CustomServer {
         for (Question q : question) {
 
             System.out.println(q);
-            System.out.println("Size before clear: " + clientHandlers.size());
+            //System.out.println("Size before clear: " + clientHandlers.size());
             clientHandlers.clear();
-            System.out.println("Size after clear: " + clientHandlers.size());
+            //System.out.println("Size after clear: " + clientHandlers.size());
 
             for (Socket client : clients) {
                 System.out.println(client);
@@ -126,19 +311,31 @@ public class CustomServer {
                 questionCountdown(timeAnswerQuest);
                 for (ClientHandler ch : clientHandlers) {
                     dos = ch.getDos();
+//                    System.out.println(ch.getClient().getPort() + ": " + ch.getResponse());
+//                    System.out.println(ch.getClient().getPort() + ": " + ch.getState());
+                    if (ch.getResponse() == null && ch.getState() == Thread.State.TERMINATED) {
+                        Socket toBeRemoved = ch.getClient();
+                        clients.remove(toBeRemoved);
+                        inStreamMap.remove(toBeRemoved);
+                        continue;
+                    }
                     dos.writeUTF("Time out");
+                    //System.out.println(ch.getDis());
                     dos.flush();
 
+
                 }
+                //System.out.println("hi");
                 clientAnswerHandler(q);
                 System.out.println("Waiting for 3 seconds to continue...");
+                System.out.println("Size: " + clientAnswers.size());
                 Thread.sleep(3000);
 
 
             } catch (InterruptedException e) {
                 e.printStackTrace();
             } catch (SocketException e) {
-                System.out.println("No client");
+                System.out.println("No more client");
             }
         }
 
@@ -149,20 +346,29 @@ public class CustomServer {
 
 
         System.out.println("Game over.");
-    }
+    } */
 
     //Get all the answers
     private static void clientAnswerHandler(Question q) {
         for (ClientHandler ch : clientHandlers) {
 
-            String ans = null;
-            while (ans == null) {
-                ans = ch.getResponse();
+            String ans = ch.getResponse();
+            String finalAnswer = ch.getClient().getPort() + ";" + q.getTitle() + ";";
+
+
+            if (ch.getState() == Thread.State.RUNNABLE) {
+                //System.out.println(ch.getClient().getPort() + "; " + ch.getState());
+                while (ans == null) {
+                    ans = ch.getResponse();
+                }
             }
-            String answer = ch.getClient().getPort() + ";" + q.getTitle() + ";" + ans;
-            clientAnswers.add(answer);
+            if (ans != null) {
+                finalAnswer += ans;
+                clientAnswers.add(finalAnswer);
+            }
 
         }
 
     }
+
 }
